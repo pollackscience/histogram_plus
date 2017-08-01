1627945fa98b54ea64ffd9ddf9d15948569da6b8
Brian Pollack <brianleepollack@gmail.com>
2017-08-01 13:25:49 -0500

    publishing to pypi, updating docs

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
 setup(name='histogram_plus',
-      version='0.1',
+      version='0.1.1',
       description='Overloaded histogram maker based on mpl',
+      long_description='Documentation: https://brovercleveland.github.io/histogram_plus/',
       url='http://github.com/brovercleveland/histogram_plus',
       author='Brian Pollack',
       author_email='brianleepollack@gmail.com',
       license='MIT',
       packages=['histogram_plus'],
+      install_requires=[ 'numpy', 'pandas', 'matplotlib'],
       zip_safe=False)
